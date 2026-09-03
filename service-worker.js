b6e4ef329721554a132d3d3cdefdaee14df5a04e
Orat28 <minooriel@gmail.com>
2026-09-03 15:40:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rifa-cache-v3"; // <--- CAMBIA ESTE NÚMERO (v2, v3, v4...) CADA VEZ QUE MODIFIQUES EL HTML/CSS
+const CACHE_NAME = "rifa-cache-v4"; // <--- CAMBIA ESTE NÚMERO (v2, v3, v4...) CADA VEZ QUE MODIFIQUES EL HTML/CSS
 
 const ASSETS = [
   "./",
